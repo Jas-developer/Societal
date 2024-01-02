a2d173c487ed1ba2aa23a7c21c3073d4df0fdaf0
Jason <devop.jas@gmail.com>
2024-01-02 21:08:39 +0800

    removed unused

--- server/server.js
+++ server/server.js
@@ -14,9 +14,7 @@ import { register } from "../server/controllers/auth.js";
 import { postRoutes } from "./routes/postRoutes.js";
 import { verifyToken } from "./middleware/auth.js";
 import { createPost } from "./controllers/posts.js";
-import { User } from "./models/User.js";
-import { Post } from "./models/Post.js";
-import { users, posts } from "./data/index.js";
+
 /*CONFIGURATIONS*/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
